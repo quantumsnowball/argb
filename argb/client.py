3396919126443e6ae62c471156ae65488f7ce316
Quantum Snowball <quantum.snowball@gmail.com>
2025-09-06 05:10:19 +0800

    Client now subclass OpenRGBClient

--- argb/client.py
+++ argb/client.py
@@ -1,29 +1,25 @@
 import time
 from subprocess import DEVNULL, Popen
+from typing import Self
 
 from openrgb import OpenRGBClient
 
 from argb.utils import PORT
 
 
-class Client:
+class Client(OpenRGBClient):
     def __init__(
         self,
         port: int = PORT,
     ) -> None:
-        self._proc: Popen | None = None
-        self._client: OpenRGBClient | None = None
-        self._port = port
-
-    def __enter__(self) -> OpenRGBClient:
         self._proc = Popen(
-            ['openrgb', '--server', f'--server-port', str(self._port)],
+            ['openrgb', '--server', f'--server-port', str(port)],
             stdout=DEVNULL,
             stderr=DEVNULL,
         )
         for _ in range(10):
             try:
-                self._client = OpenRGBClient(port=self._port)
+                super().__init__(port=port)
                 break
             except TimeoutError:
                 time.sleep(1)
@@ -31,8 +27,12 @@ class Client:
         else:
             raise TimeoutError
 
-        return self._client
+    def __enter__(self) -> Self:
+        return self
 
     def __exit__(self, type, value, traceback) -> None:
         if self._proc is not None:
             self._proc.terminate()
+
+    def stop(self) -> None:
+        self.disconnect()

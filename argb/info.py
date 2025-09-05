a09d67e458d0a0f403bc7aeefb946f29e1e5e213
Quantum Snowball <quantum.snowball@gmail.com>
2025-09-06 04:29:54 +0800

    apply to info

--- argb/info.py
+++ argb/info.py
@@ -1,13 +1,12 @@
 import click
-from openrgb import OpenRGBClient
 
-from argb.utils import PORT
+from argb.client import Client
 
 
 @click.command
 def info() -> None:
-    client = OpenRGBClient(port=PORT)
-    for i, device in enumerate(client.devices):
-        click.secho(f'{i}: {device.name} type={device.type}:', fg='yellow')
-        for j, zone in enumerate(device.zones):
-            click.secho(f'  {j}: {zone.name:>10s} type={zone.type} LEDs={len(zone.leds)}', fg='green')
+    with Client() as client:
+        for i, device in enumerate(client.devices):
+            click.secho(f'{i}: {device.name} type={device.type}:', fg='yellow')
+            for j, zone in enumerate(device.zones):
+                click.secho(f'  {j}: {zone.name:>10s} type={zone.type} LEDs={len(zone.leds)}', fg='green')

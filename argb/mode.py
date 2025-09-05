8cc98c7cabee65633a0daae29ae7f31546fafb0c
Quantum Snowball <quantum.snowball@gmail.com>
2025-09-06 03:12:31 +0800

    support unquoted names

--- argb/mode.py
+++ argb/mode.py
@@ -1,6 +1,6 @@
 import click
 from openrgb import OpenRGBClient
-from openrgb.utils import DeviceType, ModeData
+from openrgb.utils import DeviceType
 
 from argb.utils import PORT
 
@@ -11,5 +11,7 @@ def mode(mode: str) -> None:
     client = OpenRGBClient(port=PORT)
     client.clear()
     device = client.get_devices_by_type(DeviceType.MOTHERBOARD)[0]
+    for char in ('-', '_'):
+        mode = mode.replace(char, ' ')
     device.set_mode(mode, save=True)
     # device.modes[device.active_mode].speed = 255

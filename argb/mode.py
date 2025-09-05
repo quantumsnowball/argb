6daa28d6fed3b296f0dfe20f4a91047993d440ea
Quantum Snowball <quantum.snowball@gmail.com>
2025-09-06 03:03:43 +0800

    set mode as string

--- argb/mode.py
+++ argb/mode.py
@@ -1,7 +1,15 @@
 import click
+from openrgb import OpenRGBClient
+from openrgb.utils import DeviceType, ModeData
+
+from argb.utils import PORT
 
 
 @click.command
 @click.argument('mode', required=True, type=str)
 def mode(mode: str) -> None:
-    print(f'Gonna change openrgb mode to {mode} ...')
+    client = OpenRGBClient(port=PORT)
+    client.clear()
+    device = client.get_devices_by_type(DeviceType.MOTHERBOARD)[0]
+    device.set_mode(mode, save=True)
+    # device.modes[device.active_mode].speed = 255

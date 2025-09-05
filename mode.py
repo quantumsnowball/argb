49c4c6b0f36483836a199e5806987bcadc6836c2
Quantum Snowball <quantum.snowball@gmail.com>
2025-09-06 03:05:54 +0800

    cleanup

--- mode.py
+++
@@ -1,8 +0,0 @@
-from openrgb import OpenRGBClient
-from openrgb.utils import DeviceType, ModeData
-
-client = OpenRGBClient(port=6742)
-client.clear()  # Turns everything off
-device = client.get_devices_by_type(DeviceType.MOTHERBOARD)[0]
-device.set_mode('Color shift', save=True)
-device.modes[device.active_mode].speed = 255
